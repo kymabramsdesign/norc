ac4c75c0214ccd0a3fce09991c77b99888d602e9
Brandon Trumfio <brandon@kad.com>
2017-09-12 16:52:55 -0500

    ticker work

---
+++ js/ticker.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    stringsElement: '#insight-type',
+    typeSpeed: 50,
+    startDelay: 0,
+    loop: false,
+    showCursor: false
+  });
+
+  var speedUp = 100;
+  var backUp = 20;
+
+  new Typed('#typed2', {
+    strings: ['for Social<br />Change',
+    'for Social<br />Justice',
+    'for Impact',
+    'is Scalable',
+    'from<br />Evaluation',
+    'is Accessible',
+    'for Informed<br />Decisions'],
+    onStringTyped: () => {
+      speedUp = speedUp-50;
+      backUp = backUp-50;
+    },
+    backSpeed: backUp,
+    typeSpeed: speedUp,
+    backDelay: 100,
+    smartBackspace: false,
+    startDelay: 1050,
+    loop: false,
+    cursorChar: '_',
+
+  });
+
+});
+
+function toggleLoop(typed) {
+  if (typed.loop) {
+    typed.loop = false;
+  } else {
+    typed.loop = true;
+  }
+}
+
