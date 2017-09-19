e56d0b5ed25f11180c15a5922a0807ded36ad88b
Brandon Trumfio <brandon@kad.com>
2017-09-19 17:03:05 -0500

    color logo svg integration

--- js/story.js
+++ js/story.js
@@ -1,14 +1,3 @@
 $(document).ready( function() {
 
-  if ( $('body').hasClass('story-page')) {
-      var object = document.getElementById("logo");
-      var svgDocument = object.contentDocument;
-      var svgb1 = svgDocument.getElementsByClassName("tagline");
-      var svgb2 = svgDocument.getElementsByClassName("nrc");
-      var svgb2 = svgDocument.getElementsByClassName("star");
-      svgb1[0].setAttribute("fill", "yellow");
-      svgb2[0].setAttribute("fill", "red");
-      svgb3[0].setAttribute("fill", "red");
-  }
-
 });
