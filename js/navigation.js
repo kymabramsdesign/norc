0edcabb1d3c4e56b6661629edae8a8f6aa095469
Brandon Trumfio <brandon@kad.com>
2017-10-24 16:48:18 -0500

    new sections

--- js/navigation.js
+++ js/navigation.js
@@ -50,12 +50,11 @@ $(window).bind("load", function() {
     '<li class="divider"><hr /></li>' +
     '<li class="arrow"><a href="#">Economics, Markets, and the Workforce</a><img src="img/arrow.svg" alt="Arrow" class="arrow-img" />' +
     '<ul class="dropdown-menu">' +
-      '<li><a href="#">Story 1</a></li>' +
-      '<li><a href="#">Story 2</a></li>' +
-      '<li><a href="#">Story 3</a></li>' +
-      '<li><a href="#">Story 4</a></li>' +
-      '<li><a href="#">Story 5</a></li>' +
-      '<li><a href="#">Story 6</a></li>' +
+      '<li><a href="/congregations">Tracking the professional and economic health of churches</a></li>' +
+      '<li><a href="/gas-prices">Tracking gasoline and diesel prices</a></li>' +
+      '<li><a href="/poverty-research">Exploring poverty and resilience</a></li>' +
+      '<li><a href="/consumer-finances">Monitoring the financial health of American families</a></li>' +
+      '<li><a href="/youth-data">Serving diverse purposes with rich data</a></li>' +
     '</ul></li>' +
     '<li class="arrow"><a href="#">Education, Training, and Learning</a><img src="img/arrow.svg" alt="Arrow" class="arrow-img" />' +
     '<ul class="dropdown-menu">' +
@@ -84,22 +83,13 @@ $(window).bind("load", function() {
       '<li><a href="/zika-awareness">Testing Americans’ Zika awareness</a></li>' +
     '</ul></li>' +
     '<li class="arrow"><a href="#">Society, Media, and Public Affairs</a><img src="img/arrow.svg" alt="Arrow" class="arrow-img" />' +
-    '<ul class="dropdown-menu">' +
-      '<li><a href="#">Story 1</a></li>' +
-      '<li><a href="#">Story 2</a></li>' +
-      '<li><a href="#">Story 3</a></li>' +
-      '<li><a href="#">Story 4</a></li>' +
-      '<li><a href="#">Story 5</a></li>' +
-      '<li><a href="#">Story 6</a></li>' +
-    '</ul></li>' +
-    '<li class="arrow"><a href="#">Science and Innovation</a><img src="img/arrow.svg" alt="Arrow" class="arrow-img" />' +
     '<ul class="dropdown-menu last-one">' +
-      '<li><a href="#">Story 1</a></li>' +
-      '<li><a href="#">Story 2</a></li>' +
-      '<li><a href="#">Story 3</a></li>' +
-      '<li><a href="#">Story 4</a></li>' +
-      '<li><a href="#">Story 5</a></li>' +
-      '<li><a href="#">Story 6</a></li>' +
+      '<li><a href="/successful-aging">Exploring the intersection between social life and successful aging</a></li>' +
+      '<li><a href="/intimate-partner-violence">Combating intimate partner violence</a></li>' +
+      '<li><a href="/minds-and-media">Tracking American’s concerns and the media that inform them</a></li>' +
+      '<li><a href="/teen-surveys">Tapping into the minds and behaviors of teens</a></li>' +
+      '<li><a href="/disaster-fatalities">Understanding how disasters kill</a></li>' +
+      '<li><a href="/smoking-and-social-media">Mapping smoking policies and behavior</a></li>' +
     '</ul></li>' +
     '<li class="divider"><hr /></li>' +
     '<li class="arrow"><a href="#">Leadership and Trustees</a></li>' +
