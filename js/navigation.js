4bd968f6616b9ece73d7ad9f9c719a22c6dc7ed8
Brandon Trumfio <brandon@kad.com>
2017-10-24 11:16:50 -0500

    menu changes

--- js/navigation.js
+++ js/navigation.js
@@ -23,6 +23,9 @@ $('.menu-button').on('click', function() {
     $(this).css('z-index',300).addClass('menu-open').attr('src', 'img/menu-button-orange.svg');
     $('.menu-container').addClass('moved');
     $("body").addClass('no-scroll'); // body to stop scroll
+    $('.arrow a').removeClass('active');
+    $('.arrow-img').attr('src', 'img/arrow.svg').removeClass('open');
+    $('.dropdown-menu').slideUp(200);
   }
 });
 
@@ -32,6 +35,9 @@ $('.white-page-overlay').on('click', function() {
   $('.menu-button').removeClass('menu-open').attr('src', 'img/menu-button.svg');
   $('.menu-container').removeClass('moved');
   $("body").removeClass('no-scroll'); // body to scroll again
+  $('.arrow a').removeClass('active');
+  $('.arrow-img').attr('src', 'img/arrow.svg').removeClass('open');
+  $('.dropdown-menu').slideUp(200);
 });
 
 // Wait until page is fully loaded to initialize the menu
@@ -62,11 +68,11 @@ $(window).bind("load", function() {
     '</ul></li>' +
     '<li class="arrow"><a href="#">Global Development</a><img src="img/arrow.svg" alt="Arrow" class="arrow-img" />' +
     '<ul class="dropdown-menu">' +
-      '<li><a href="/usaid-evaluations.html">Measuring impact and improving performance</a></li>' +
-      '<li><a href="/complex-projects.html">Assessing the impact of complex projects</a></li>' +
-      '<li><a href="/real-time-adaptability.html">Adapting timelines and methodologies to conditions on the ground</a></li>' +
-      '<li><a href="/vulnerable-populations.html">Reaching vulnerable populations in fragile environments</a></li>' +
-      '<li><a href="/closed-societies.html">Peering into closed societies</a></li>' +
+      '<li><a href="/usaid-evaluations">Measuring impact and improving performance</a></li>' +
+      '<li><a href="/complex-projects">Assessing the impact of complex projects</a></li>' +
+      '<li><a href="/real-time-adaptability">Adapting timelines and methodologies to conditions on the ground</a></li>' +
+      '<li><a href="/vulnerable-populations">Reaching vulnerable populations in fragile environments</a></li>' +
+      '<li><a href="/closed-societies">Peering into closed societies</a></li>' +
     '</ul></li>' +
     '<li class="arrow"><a href="#">Health and Wellbeing</a><img src="img/arrow.svg" alt="Arrow" class="arrow-img" />' +
     '<ul class="dropdown-menu">' +
@@ -87,7 +93,7 @@ $(window).bind("load", function() {
       '<li><a href="#">Story 6</a></li>' +
     '</ul></li>' +
     '<li class="arrow"><a href="#">Science and Innovation</a><img src="img/arrow.svg" alt="Arrow" class="arrow-img" />' +
-    '<ul class="dropdown-menu">' +
+    '<ul class="dropdown-menu last-one">' +
       '<li><a href="#">Story 1</a></li>' +
       '<li><a href="#">Story 2</a></li>' +
       '<li><a href="#">Story 3</a></li>' +
@@ -97,7 +103,7 @@ $(window).bind("load", function() {
     '</ul></li>' +
     '<li class="divider"><hr /></li>' +
     '<li class="arrow"><a href="#">Leadership and Trustees</a></li>' +
-    '<li><a href="#">GAIN MORE INSIGHT</a></li>' +
+    '<li><a href="#">Gain More Insight</a></li>' +
     '</ul>'
   );
 
