bf133dc7216eb1dd048e4b2b5e7ced8243835ef5
Brandon Trumfio <brandon@kad.com>
2018-02-14 15:27:30 -0600

    ie10 and ie11 popup fixes

--- js/story.js
+++ js/story.js
@@ -566,6 +566,17 @@ function checkBrowser() {
   if ( isChromium === true ) {
     $('.main p, .area .h2 p, .main .section-title').css('font-weight', '300');
   }
+
+  var isIE11 = '-ms-scroll-limit' in document.documentElement.style && '-ms-ime-align' in document.documentElement.style;
+  if ( isIE11 ) {
+    $('.main-image').addClass('ie11');
+  }
+
+  var ieVersion = /*@cc_on (function() {switch(@_jscript_version) {case 1.0: return 3; case 3.0: return 4; case 5.0: return 5; case 5.1: return 5; case 5.5: return 5.5; case 5.6: return 6; case 5.7: return 7; case 5.8: return 8; case 9: return 9; case 10: return 10;}})() || @*/ 0;
+  console.log(ieVersion);
+  if ( ieVersion === 10 ) {
+    $('.left-50 .picture, .left-50 .chart').addClass('ie10');
+  }
 }
 
 
